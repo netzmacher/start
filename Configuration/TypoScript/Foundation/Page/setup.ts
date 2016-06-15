3b045fe9c76e1e0e60f10deb6ca58dce25829992
Dirk Wildt <wildt@die-netzmacher.de>
2016-06-15 23:41:43 +0200

    2.4.3

--- Configuration/TypoScript/Foundation/Page/setup.ts
+++ Configuration/TypoScript/Foundation/Page/setup.ts
@@ -1,19 +1,19 @@
 page {
   includeCSS {
-    startNormalize  = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/css/normalize.css
-    startFoundation = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/css/foundation.min.css
+    startNormalize  = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/css/normalize.css
+    startFoundation = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/css/foundation.min.css
   }
   includeJSlibs {
-    startModernizr = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/js/vendor/modernizr.js
+    startModernizr = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/vendor/modernizr.js
   }
   includeJSFooter {
-    startFastclick          = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/js/vendor/fastclick.js
-    startFoundation         = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/js/foundation.min.js
-    startFoundationClearing = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/js/foundation/foundation.clearing.js
+    startFastclick          = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/vendor/fastclick.js
+    startFoundation         = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/foundation.min.js
+    startFoundationClearing = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/foundation/foundation.clearing.js
     startFoundationInit     = EXT:start/Resources/Public/JavaScript/Libs/foundation.js
   }
   includeJSFooterlibs {
-    startJquery = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/js/vendor/jquery.js
+    startJquery = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/vendor/jquery.js
     startJquery {
       forceOnTop = 1
     }
@@ -23,20 +23,20 @@ page {
   // #71476
 startPrint {
   includeCSS {
-    startNormalize  = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/css/normalize.css
-    startFoundation = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/css/foundation.min.css
+    startNormalize  = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/css/normalize.css
+    startFoundation = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/css/foundation.min.css
   }
   includeJSlibs {
-    startModernizr = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/js/vendor/modernizr.js
+    startModernizr = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/vendor/modernizr.js
   }
   includeJSFooter {
-    startFastclick          = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/js/vendor/fastclick.js
-    startFoundation         = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/js/foundation.min.js
-    startFoundationClearing = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/js/foundation/foundation.clearing.js
+    startFastclick          = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/vendor/fastclick.js
+    startFoundation         = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/foundation.min.js
+    startFoundationClearing = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/foundation/foundation.clearing.js
     startFoundationInit     = EXT:start/Resources/Public/JavaScript/Libs/foundation.js
   }
   includeJSFooterlibs {
-    startJquery = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.1/js/vendor/jquery.js
+    startJquery = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/vendor/jquery.js
     startJquery {
       forceOnTop = 1
     }
--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
   'title' => 'Start TYPO3 Responsive!',
-  'description' => 'Start TYPO3 Responsive! (start) extends TYPO3 with the responsive framework Foundation (Zurb), '
-  . 'a dozen responsive Backend Layouts and some responsive Grid Elements. '
-  . 'It enables editors, to create responsive nested layouts within a few minutes. '
-  . 'It extends some backend forms for controlling the display of content elements depending on devices like smartphones, tablets and desktops. '
-  . 'It extends TYPO3 content elements and menus with some helpful responsive Foundation features. '
-  . 'A template for a responsive newsletter is included. '
-  . 'Manual: http://start-typo3-responsive.de/typo3conf/ext/start/doc/manual.pdf. '
-  ,
+  'description' => 'Start TYPO3 Responsive! (start) extends TYPO3 with the responsive framework Foundation (Zurb), a dozen responsive Backend Layouts and some responsive Grid Elements. It enables editors, to create responsive nested layouts within a few minutes. It extends some backend forms for controlling the display of content elements depending on devices like smartphones, tablets and desktops. It extends TYPO3 content elements and menus with some helpful responsive Foundation features. A template for a responsive newsletter is included. Manual: http://start-typo3-responsive.de/typo3conf/ext/start/doc/manual.pdf. ',
   'category' => 'be',
   'author' => 'Dirk Wildt (Die Netzmacher)',
   'author_email' => 'http://wildt.at.die-netzmacher.de',
@@ -18,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array(
   'uploadfolder' => '0',
   'createDirs' => '',
   'clearCacheOnLoad' => 1,
-  'version' => '2.4.2',
+  'version' => '2.4.3',
   'constraints' => array(
     'depends' => array(
       'typo3' => '6.2.0-7.6.99',
