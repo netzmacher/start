7b0273c4d2dd76839c4a7ee6e2bbb0dc11e5db88
Dirk Wildt <wildt@die-netzmacher.de>
2016-07-13 22:20:28 +0200

    2.4.5

--- Configuration/TypoScript/Foundation/Start/constants.ts
+++ Configuration/TypoScript/Foundation/Start/constants.ts
@@ -127,15 +127,15 @@ start {
       searchform = 
       # cat=start - foundation - topbar//301;  type=+int;                   label= Search form:[OBLIGATE!] Id of the result page
       searchform.pid = 1
-      # cat=start - foundation - topbar//301;  type=string;                 label= Search form:[OBLIGATE!] Name of the parameter
+      # cat=start - foundation - topbar//302;  type=string;                 label= Search form:[OBLIGATE!] Name of the parameter
       searchform.param = sword
-      # cat=start - foundation - topbar//301;  type=options[get,post];      label= Search form:Method
+      # cat=start - foundation - topbar//303;  type=options[get,post];      label= Search form:Method
       searchform.method = post
-      # cat=start - foundation - topbar//301;  type=options[left,right];    label= Search form:Position of the search form
+      # cat=start - foundation - topbar//304;  type=options[left,right];    label= Search form:Position of the search form
       searchform.position = right
-      # cat=start - foundation - topbar//302;  type=string;                 label= Search form:Placeholder
+      # cat=start - foundation - topbar//305;  type=string;                 label= Search form:Placeholder
       searchform.placeholder = Find Stuff
-      # cat=start - foundation - topbar//303;  type=string;                 label= Search form:Label of the button
+      # cat=start - foundation - topbar//306;  type=string;                 label= Search form:Label of the button
       searchform.button = Search
     }
   }
