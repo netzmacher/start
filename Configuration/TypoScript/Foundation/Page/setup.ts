35d3e90ea88e5c9aab5850b91e455d9dcea5c135
Dirk Wildt <wildt@die-netzmacher.de>
2017-03-20 11:23:33 +0100

    2.5.1

--- Configuration/TypoScript/Foundation/Page/setup.ts
+++ Configuration/TypoScript/Foundation/Page/setup.ts
@@ -6,16 +6,56 @@ page {
   }
   includeJSlibs {
     startModernizr = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/vendor/modernizr.js
+    startModernizr {
+				// if.isTrue start.foundation.javascript.includeJS.startModernizr (here: {$start.foundation.javascript.includeJS.startModernizr})
+			if =
+			if {
+				isTrue = {$start.foundation.javascript.includeJS.startModernizr}
+			}
+    }
   }
   includeJSFooter {
     startFastclick          = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/vendor/fastclick.js
+    startFastclick {
+				// if.isTrue start.foundation.javascript.includeJS.startFastclick (here: {$start.foundation.javascript.includeJS.startFastclick})
+			if =
+			if {
+				isTrue = {$start.foundation.javascript.includeJS.startFastclick}
+			}
+    }
     startFoundation         = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/foundation.min.js
+    startFoundation {
+				// if.isTrue start.foundation.javascript.includeJS.startFoundation (here: {$start.foundation.javascript.includeJS.startFoundation})
+			if =
+			if {
+				isTrue = {$start.foundation.javascript.includeJS.startFoundation}
+			}
+    }
     startFoundationClearing = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/foundation/foundation.clearing.js
+    startFoundationClearing {
+				// if.isTrue start.foundation.javascript.includeJS.startFoundationClearing (here: {$start.foundation.javascript.includeJS.startFoundationClearing})
+			if =
+			if {
+				isTrue = {$start.foundation.javascript.includeJS.startFoundationClearing}
+			}
+    }
     startFoundationInit     = EXT:start/Resources/Public/JavaScript/Libs/foundation.js
+    startFoundationInit {
+				// if.isTrue start.foundation.javascript.includeJS.startFoundationInit (here: {$start.foundation.javascript.includeJS.startFoundationInit})
+			if =
+			if {
+				isTrue = {$start.foundation.javascript.includeJS.startFoundationInit}
+			}
+    }
   }
   includeJSFooterlibs {
     startJquery = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/vendor/jquery.js
     startJquery {
+				// if.isTrue start.foundation.javascript.includeJS.startJquery (here: {$start.foundation.javascript.includeJS.startJquery})
+			if =
+			if {
+				isTrue = {$start.foundation.javascript.includeJS.startJquery}
+			}
       forceOnTop = 1
     }
   }
@@ -29,16 +69,56 @@ startPrint {
   }
   includeJSlibs {
     startModernizr = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/vendor/modernizr.js
+    startModernizr {
+				// if.isTrue start.foundation.javascript.includeJS.startModernizr (here: {$start.foundation.javascript.includeJS.startModernizr})
+			if =
+			if {
+				isTrue = {$start.foundation.javascript.includeJS.startModernizr}
+			}
+    }
   }
   includeJSFooter {
     startFastclick          = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/vendor/fastclick.js
+    startFastclick {
+				// if.isTrue start.foundation.javascript.includeJS.startFastclick (here: {$start.foundation.javascript.includeJS.startFastclick})
+			if =
+			if {
+				isTrue = {$start.foundation.javascript.includeJS.startFastclick}
+			}
+    }
     startFoundation         = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/foundation.min.js
+    startFoundation {
+				// if.isTrue start.foundation.javascript.includeJS.startFoundation (here: {$start.foundation.javascript.includeJS.startFoundation})
+			if =
+			if {
+				isTrue = {$start.foundation.javascript.includeJS.startFoundation}
+			}
+    }
     startFoundationClearing = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/foundation/foundation.clearing.js
+    startFoundationClearing {
+				// if.isTrue start.foundation.javascript.includeJS.startFoundationClearing (here: {$start.foundation.javascript.includeJS.startFoundationClearing})
+			if =
+			if {
+				isTrue = {$start.foundation.javascript.includeJS.startFoundationClearing}
+			}
+    }
     startFoundationInit     = EXT:start/Resources/Public/JavaScript/Libs/foundation.js
+    startFoundationInit {
+				// if.isTrue start.foundation.javascript.includeJS.startFoundationInit (here: {$start.foundation.javascript.includeJS.startFoundationInit})
+			if =
+			if {
+				isTrue = {$start.foundation.javascript.includeJS.startFoundationInit}
+			}
+    }
   }
   includeJSFooterlibs {
     startJquery = EXT:start/Resources/Public/JavaScript/Libs/Foundation-5.5.3/js/vendor/jquery.js
     startJquery {
+				// if.isTrue start.foundation.javascript.includeJS.startJquery (here: {$start.foundation.javascript.includeJS.startJquery})
+			if =
+			if {
+				isTrue = {$start.foundation.javascript.includeJS.startJquery}
+			}
       forceOnTop = 1
     }
   }
--- Configuration/TypoScript/Foundation/Start/Constants/Foundation/_constants.ts
+++ Configuration/TypoScript/Foundation/Start/Constants/Foundation/_constants.ts
@@ -1,4 +1,5 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Foundation/Start/Constants/Foundation/all.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Foundation/Start/Constants/Foundation/iconbarStart.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Foundation/Start/Constants/Foundation/javascript.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Foundation/Start/Constants/Foundation/thumbnails.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Foundation/Start/Constants/Foundation/Topbar/constants.ts">
