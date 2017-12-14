91d71b8616f71621f51254e702f51c1f31299670
Dirk Wildt <wildt@die-netzmacher.de>
2017-12-14 21:15:16 +0100

    3.0.2

--- Configuration/TypoScript/Base/Config/constants/config/debugging.ts
+++ Configuration/TypoScript/Base/Config/constants/config/debugging.ts
@@ -1,10 +1,10 @@
 start {
 
   config {
-    # cat=start - debugging//105; type=boolean;  label= contentObjectExceptionHandler:Disable it, if you like to see error prompts for content objects instead of an exception handling. Error prompts will cached. Please enable exception handling after the debugging.
+    # cat=start - config - debugging//105; type=boolean;  label= contentObjectExceptionHandler:Disable it, if you like to see error prompts for content objects instead of an exception handling. Error prompts will cached. Please enable exception handling after the debugging.
     contentObjectExceptionHandler     = 1
 		contentObjectExceptionHandler {
-	    # cat=start - debugging//106; type=string;  label= contentObjectExceptionHandler prompt:Error message in case of exception handling
+	    # cat=start - config - debugging//106; type=string;  label= contentObjectExceptionHandler prompt:Error message in case of exception handling
 			errorMessage = Oops an error occurred. Code: %s. If you like to know more, please disable the ContentObjectExceptionHandler @ Constant Editor > category [START - DEBUGGING]
 		}
   }
