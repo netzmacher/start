064d8d72aa579e159d58ad720ff73dfb46249698
Dirk Wildt <wildt@die-netzmacher.de>
2019-02-01 20:39:54 +0100

    4.5.5

--- Configuration/TypoScript/Base/Page/Page/63148.ts
+++ Configuration/TypoScript/Base/Page/Page/63148.ts
@@ -220,7 +220,7 @@ page {
       }
     }
       // Three rows: header | content
-    start__silver_05 < .start__bronze_04
+    start__silver_05 < .start__silver_04
     start__silver_05 {
       file = {$start.fluid.path.templates}silver_05.html
       settings {
@@ -228,7 +228,7 @@ page {
       }
     }
       // Three rows: content | bottom
-    start__silver_06 < .start__bronze_04
+    start__silver_06 < .start__silver_04
     start__silver_06 {
       file = {$start.fluid.path.templates}silver_06.html
       settings {
