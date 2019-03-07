12abbe9cad0ec4008400696f01d8bdc08fdd5b9b
Dirk Wildt <wildt@die-netzmacher.de>
2019-03-07 02:47:18 +0100

    4.6.2

--- Configuration/TypoScript/Base/Start/Gridelements/constants/gridelements.ts
+++ Configuration/TypoScript/Base/Start/Gridelements/constants/gridelements.ts
@@ -1,18 +1,25 @@
 start {
 
-  # cat=start - gridelements//101; type=int+; label= Id 7:Id of the gridelement with accordion or tabs
+  gridelement {
+		#	cat=start - gridelements - background//201; type=string; label= Class:CSS class of the background
+	  background.class = background-position: center;background-repeat: no-repeat;background-size: cover;
+		#	cat=start - gridelements - background//302; type=int+; label= width:Maximum width of the image
+	  background.width = 1200
+	}
+
+  # cat=start - gridelements - id//101; type=int+; label= Id 7:Id of the gridelement with accordion or tabs
   gridelement.accordionOrTabs = 7
-  # cat=start - gridelements//102; type=int+; label= Id 1:Id of the gridelement with 1 column
+  # cat=start - gridelements - id//102; type=int+; label= Id 1:Id of the gridelement with 1 column
   gridelement.col1 = 1
-  # cat=start - gridelements//103; type=int+; label= Id 2:Id of the gridelement with 2 columns
+  # cat=start - gridelements - id//103; type=int+; label= Id 2:Id of the gridelement with 2 columns
   gridelement.col2 = 2
-  # cat=start - gridelements//104; type=int+; label= Id 3:Id of the gridelement with 3 columns
+  # cat=start - gridelements - id//104; type=int+; label= Id 3:Id of the gridelement with 3 columns
   gridelement.col3 = 3
-  # cat=start - gridelements//105; type=int+; label= Id 4:Id of the gridelement with 4 columns
+  # cat=start - gridelements - id//105; type=int+; label= Id 4:Id of the gridelement with 4 columns
   gridelement.col4 = 4
-  # cat=start - gridelements//106; type=int+; label= Id 5:Id of the gridelement with 5 columns
+  # cat=start - gridelements - id//106; type=int+; label= Id 5:Id of the gridelement with 5 columns
   gridelement.col5 = 5
-  # cat=start - gridelements//107; type=int+; label= Id 6:Id of the gridelement with 6 columns
+  # cat=start - gridelements - id//107; type=int+; label= Id 6:Id of the gridelement with 6 columns
   gridelement.col6 = 6
 
 }
---
+++ Configuration/TypoScript/Base/tt_content/_constants.ts
@@ -0,0 +1,1 @@
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/tt_content/Fluid_Styled_Content/constants.ts">
