f860ba876de404b96aba6d0f3d11b1419b368e56
Dirk Wildt <wildt@die-netzmacher.de>
2019-03-21 21:56:35 +0100

    4.7.1

--- Configuration/TypoScript/Foundation/Ext/_constants.ts
+++ Configuration/TypoScript/Foundation/Ext/_constants.ts
@@ -1,1 +1,2 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Foundation/Ext/CbFoundation/constants.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Foundation/Ext/t3foundation/constants.ts">
---
+++ Configuration/TypoScript/Foundation/Ext/t3foundation/constants.ts
@@ -0,0 +1,3 @@
+plugin.tx_t3foundation.settings.js.initscript = 0
+plugin.tx_t3foundation.settings.js.foundation = 0
+plugin.tx_t3foundation.settings.js.modernizr = 0
