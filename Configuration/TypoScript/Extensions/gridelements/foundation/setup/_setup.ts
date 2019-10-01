c4d1321813ab652ea66ea72091740d48608e210d
Dirk Wildt <wildt@die-netzmacher.de>
2019-10-01 21:35:06 +0200

    5.1.14

--- Configuration/TypoScript/Extensions/gridelements/foundation/setup/_setup.ts
+++ Configuration/TypoScript/Extensions/gridelements/foundation/setup/_setup.ts
@@ -4,7 +4,8 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Extensions/gridelements/foundation/setup/columns_04.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Extensions/gridelements/foundation/setup/columns_05.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Extensions/gridelements/foundation/setup/columns_06.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Extensions/gridelements/foundation/setup/foundation_orbit.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Extensions/gridelements/foundation/setup/foundation_section.ts">
+// #i0258, 191001, dwildt, 2-
+//<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Extensions/gridelements/foundation/setup/foundation_orbit.ts">
+//<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Extensions/gridelements/foundation/setup/foundation_section.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Extensions/gridelements/foundation/setup/accordionOrTabs.ts">
 
