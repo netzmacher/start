c3fc02c1060b34dc40e5425b586dd23d11e05c27
Dirk Wildt <wildt@die-netzmacher.de>
2019-10-02 18:04:53 +0200

    5.1.15

---
+++ Configuration/TypoScript/Base/tt_content/Fluid_Styled_Content/8.7/_setup.ts
@@ -0,0 +1,3 @@
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/tt_content/Fluid_Styled_Content/8.7/tt_content.gridelements_pi1.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/tt_content/Fluid_Styled_Content/8.7/tt_content.stdWrap.innerWrap.cObject.ts">
+
--- Configuration/TypoScript/Base/tt_content/Fluid_Styled_Content/_setup.ts
+++ Configuration/TypoScript/Base/tt_content/Fluid_Styled_Content/_setup.ts
@@ -1,2 +1,6 @@
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/tt_content/Fluid_Styled_Content/tt_content.gridelements_pi1.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/tt_content/Fluid_Styled_Content/tt_content.stdWrap.innerWrap.cObject.ts">
+[compatVersion = 9.5.0]
+	// TYPO3 >= 9.5.0. Internal: #i0259
+  <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/tt_content/Fluid_Styled_Content/9.5/_setup.ts">
+[else]
+  <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/tt_content/Fluid_Styled_Content/8.7/_setup.ts">
+[global]
