460595016863f95c326473c1c3673beb324c85ba
Dirk Wildt <wildt@die-netzmacher.de>
2019-10-02 17:47:05 +0200

    5.1.15

--- Configuration/TypoScript/Base/tt_content/_setup.ts
+++ Configuration/TypoScript/Base/tt_content/_setup.ts
@@ -3,8 +3,8 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/tt_content/menu.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/tt_content/txStartPagemedia.ts">
 
-[globalVar = LIT:tt_content = {$styles.content.shortcut.tables}]
-	// condition meets, if TypoScript template FluidStyledContent is included. Internal: #t1827, #i0192
+[globalVar = LIT:tt_content = {$styles.content.shortcut.tables}] || [compatVersion = 9.5.0]
+	// condition meets, if TypoScript template FluidStyledContent is included or TYPO3 version is equal or greater than 9.5.0. Internal: #t1827, #i0192, #i0259
   // Then $styles.content.shortcut.tables returns tt_content
   <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/tt_content/Fluid_Styled_Content/_setup.ts">
 [else]
