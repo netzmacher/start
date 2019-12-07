e3b50acdfaed9844e4f19035e632bb3d867fdd56
Dirk Wildt <wildt@die-netzmacher.de>
2019-12-07 23:12:04 +0100

    5.3.10

---
+++ Configuration/TypoScript/Bootstrap/Fonts/Page/includeCSS.ts
@@ -0,0 +1,10 @@
+page {
+		// empty statement for proper comments only
+  includeCSS {
+	}
+		// startBootstrapFonts
+  includeCSS =
+  includeCSS {
+    startBootstrapFonts	= EXT:start/Resources/Public/Css/Bootstrap/fonts.css
+  }
+}
