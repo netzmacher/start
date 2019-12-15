895243efd563c8ac4b689afe3f4616cf2ba3d2cc
Dirk Wildt <wildt@die-netzmacher.de>
2019-12-15 01:01:24 +0100

    5.0.3

--- Configuration/TypoScript/Base/Start/Lib/languageWithText.ts
+++ Configuration/TypoScript/Base/Start/Lib/languageWithText.ts
@@ -11,33 +11,49 @@ start {
   lib =
   lib {
       // Display languageWithText, if current page isn't root page
-    languageWithText = HMENU
+    languageWithText = COA
     languageWithText {
-      special = language
-      special {
-        value = 0,1,2
-      }
-      wrap = <nav class="languageWithText">|</nav>
-      1 = TMENU
-      1 {
-        noBlur = 1
-        NO = 1
-        NO {
-					value = de || en || fr
-          ATagTitle {
-            field = description // abstract // subtitle // title
-          }
-          allWrap = |
-          stdWrap {
-            htmlSpecialChars = 1
-          }
-        }
-        CUR < .NO
-        CUR = 1
-        CUR {
-          ATagParams = class="current"
-        }
-      }
+			20 = HMENU
+			20 {
+					special = language
+					special.value = 0,1,2
+					special.normalWhenNoLanguage = 0
+					wrap =
+					1 = TMENU
+					1 {
+							noBlur = 1
+							NO = 1
+							NO {
+									doNotLinkIt = 1
+									linkWrap = <li>|</li>
+									stdWrap.override = English || Deutsch || Dansk
+									stdWrap {
+											typolink {
+													parameter.data = page:uid
+													additionalParams = &L=0 || &L=1 || &L=2
+													ATagParams = hreflang="en-GB" || hreflang="de-DE" || hreflang="da-DK"
+													addQueryString = 1
+													addQueryString.exclude = L,id,no_cache
+													addQueryString.method = GET
+													no_cache = 0
+											}
+									}
+							}
+							ACT < .NO
+							ACT.linkWrap = <li class="active">|</li>
+							USERDEF1 < .NO
+							USERDEF1 {
+									linkWrap = <li class="text-muted">|</li>
+									stdWrap.typolink >
+							}
+							USERDEF2 < .ACT
+							USERDEF2 {
+									linkWrap = <li class="text-muted">|</li>
+									stdWrap.typolink >
+							}
+					}
+			}
+			wrap = <ul id="language_menu" class="language-menu">|</ul>
     }
   }
 }
