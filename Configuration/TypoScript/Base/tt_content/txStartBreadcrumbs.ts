8a4b94901330affc944a070d449f8d3440f245de
Dirk Wildt <wildt@die-netzmacher.de>
2019-12-18 16:39:03 +0100

    5.4.0

--- Configuration/TypoScript/Base/tt_content/txStartBreadcrumbs.ts
+++ Configuration/TypoScript/Base/tt_content/txStartBreadcrumbs.ts
@@ -1,18 +1,6 @@
 tt_content.txStartBreadcrumbs = FLUIDTEMPLATE
 tt_content.txStartBreadcrumbs {
 		// if rootpage uid isn't current page uid
-	if =
-	if {
-		value {
-				// rootpage uid
-			data = leveluid:{$start.navigation.entryLevel}
-		}
-		equals {
-				// current page uid
-			data = page:uid
-		}
-		negate = 1
-	}
 	templateName = Breadcrumbs
 	templateRootPaths {
 		10 = EXT:fluid_styled_content/Resources/Private/Templates/
@@ -30,9 +18,22 @@ tt_content.txStartBreadcrumbs {
 	dataProcessing {
 		10 = TYPO3\CMS\Frontend\DataProcessing\MenuProcessor
 		10 {
+			if =
+			if {
+				value {
+						// rootpage uid
+					data = leveluid:{$start.navigation.entryLevel}
+				}
+				equals {
+						// current page uid
+					data = page:uid
+				}
+				negate = 1
+			}
       special = rootline
       special {
         range = {$start.foundation.breadcrumbs.special.range}
+				range = 0|-1
       }
       includeNotInMenu = {$start.foundation.breadcrumbs.includeNotInMenu}
 		}
