79fde25f137f19336489aea9093a144d351f4fd4
Dirk Wildt <wildt@die-netzmacher.de>
2020-02-24 21:08:37 +0100

    5.4.5

--- Configuration/TypoScript/Base/tt_content/txStartBreadcrumbs.ts
+++ Configuration/TypoScript/Base/tt_content/txStartBreadcrumbs.ts
@@ -2,18 +2,26 @@ tt_content.txStartBreadcrumbs = FLUIDTEMPLATE
 tt_content.txStartBreadcrumbs {
 		// if rootpage uid isn't current page uid
 	templateName = Breadcrumbs
+		// 10 = EXT:fluid_styled_content/Resources/Private/Templates/. 20 = EXT:start/Resources/Private/Templates/foundation-5.5.3/Templates/Navigation/. 30 = {$start.fluid.path.navigation.templates}
+	templateRootPaths =
 	templateRootPaths {
 		10 = EXT:fluid_styled_content/Resources/Private/Templates/
-		20 = {$start.fluid.path.navigation.templates}
-		//10 = EXT:fluid_styled_content/Resources/Private/Layouts/
+		20 = EXT:start/Resources/Private/Templates/foundation-5.5.3/Templates/Navigation/
+		30 = {$start.fluid.path.navigation.templates}
 	}
+		// 10 = EXT:fluid_styled_content/Resources/Private/Layouts/. 20 = EXT:start/Resources/Private/Templates/foundation-5.5.3/Layouts/Navigation/. 30 = {$start.fluid.path.navigation.layouts}
+	layoutRootPaths =
 	layoutRootPaths {
 		10 = EXT:fluid_styled_content/Resources/Private/Layouts/
-		20 = {$start.fluid.path.navigation.layouts}
+		20 = EXT:start/Resources/Private/Templates/foundation-5.5.3/Layouts/Navigation/
+		30 = {$start.fluid.path.navigation.layouts}
 	}
+		// 10 = EXT:fluid_styled_content/Resources/Private/Partials/. 20 = EXT:start/Resources/Private/Templates/foundation-5.5.3/Partials/Navigation/. 30 = {$start.fluid.path.navigation.partials}
+	partialRootPaths =
 	partialRootPaths {
-		0 = EXT:fluid_styled_content/Resources/Private/Partials/
-		10 = {$start.fluid.path.navigation.partials}
+		10 = EXT:fluid_styled_content/Resources/Private/Partials/
+		20 = EXT:start/Resources/Private/Templates/foundation-5.5.3/Partials/Navigation/
+		30 = {$start.fluid.path.navigation.partials}
 	}
 	dataProcessing {
 		10 = TYPO3\CMS\Frontend\DataProcessing\MenuProcessor
