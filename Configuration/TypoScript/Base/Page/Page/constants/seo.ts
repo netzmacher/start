3d2a57805e8ffe6ee028b78dc0118cfe2ba97b02
Dirk Wildt <wildt@die-netzmacher.de>
2020-07-18 13:27:58 +0200

    8.0.5

--- Configuration/TypoScript/Base/Page/Page/constants/seo.ts
+++ Configuration/TypoScript/Base/Page/Page/constants/seo.ts
@@ -6,7 +6,7 @@ start {
       author      = Start TYPO3 Responsive!
       # cat=start - search engine optimisation//101;  type=string; label= description:Default description.
       description = Start TYPO3 Responsive! And you'll get a responsive TYPO3 in 15 minutes - based on the responsive framework Foundation (Zurb)
-      # cat=start - search engine optimisation//102;  type=string; label= description:Default keywords.
+      # cat=start - search engine optimisation//102;  type=string; label= keywords:Default keywords.
       keywords    = TYPO3, Start, responsive, Foundation
     }
   }
