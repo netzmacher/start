3145acfb5e3d4cbaa27b4de2d9ca198901db1113
Dirk Wildt <dirk.wildt@verdigado.com>
2020-11-23 10:43:05 +0100

    [FEATURE] Crop for items in the top bar

--- Configuration/TypoScript/Foundation/Start/Constants/Foundation/Topbar/constants.ts
+++ Configuration/TypoScript/Foundation/Start/Constants/Foundation/Topbar/constants.ts
@@ -3,11 +3,13 @@ start {
     topbar {
       # cat=start - foundation - topbar*//100;  type=string;                 label= Name*:[OBLIGATE!] Your company name. I.e: Die Netzmacher. Will be overriden by corresponding page property. If both are empty – page property and this property, name won't displayed.
       name                = TYPO3 Start
-      # cat=start - foundation - topbar*//100;  type=string;                 label= Title*:[OBLIGATE!] Title tag of the top bar name. I.e: TYPO3 agency
+      # cat=start - foundation - topbar*//101;  type=string;                 label= Title*:[OBLIGATE!] Title tag of the top bar name. I.e: TYPO3 agency
       title               = Start TYPO3 Responsive! (start)
-      # cat=start - foundation - topbar*//110;  type=string;                 label= Icon label:The icon lable is displayed in minimised menus.
+      # cat=start - foundation - topbar*//102;  type=+int;                   label= Crop:Maximum length of a menu item
+      crop								= 40
+      # cat=start - foundation - topbar*//110;  type=string;                 label= Icon label:The icon label is displayed in minimised menus.
       icon.label.default  = Menu
-      # cat=start - foundation - topbar*//111;  type=string;                 label= Icon label (German):The icon lable for minimised menus in German language.
+      # cat=start - foundation - topbar*//111;  type=string;                 label= Icon label (German):The icon label for minimised menus in German language.
       icon.label.de       = Menü
       # cat=start - foundation - topbar*//200;  type=options[contain-to-grid,contain-to-grid fixed,fixed,contain-to-grid vertical];  label= Top bar position: Full-browser width by default. To make the top bar stay fixed as you scroll, select "fixed". If you want your navigation to be set to your grid width, wrap it with "contain-to-grid". You may use fixed and contain-to-grid together.
       position            = contain-to-grid
