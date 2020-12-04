b2f591bc60b2e45cd41cc8f059d2274c8770af7e
Fredrik Boström <fredrik.bostroem@verdigado.com>
2020-12-04 16:49:23 +0100

    headlineimage

---
+++ Configuration/TCA/Overrides/tt_content/headlineimage.php
@@ -0,0 +1,46 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+	'tt_content',
+	'CType',
+	[
+		'LLL:EXT:start/Configuration/TCA/Overrides/tt_content/headlineimage.xlf:headlineimage.title',
+		'txStartHeadlineimage',
+		'ext-start-headlineimage',
+	],
+	'textmedia',
+	'after'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    // 'list_type' does not apply here
+    '*',
+    // Flexform configuration schema file
+    'FILE:EXT:start/Configuration/FlexForm/startHeadlineimage.xml',
+    // ctype
+    'txStartHeadlineimage'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['txStartHeadlineimage'] = [
+	'showitem' => '
+	--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+		--palette--;;general,
+		--palette--;;headers,
+		pi_flexform,
+	--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.images,
+		image,
+	--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+		--palette--;;frames,
+		--palette--;;appearanceLinks,
+	--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+		--palette--;;language,
+	--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+		--palette--;;hidden,
+		--palette--;;access,
+	--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+		categories,
+	--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+		rowDescription,
+	--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+	',
+];
