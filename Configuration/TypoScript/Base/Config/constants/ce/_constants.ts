0c3c6474c993ab4bf5a250ecce5872d4fd2f0ea5
Dirk Wildt <dirk.wildt@verdigado.com>
2020-12-16 19:25:20 +0100

    [TASK] headlineimage #394

--- Configuration/TypoScript/Base/Config/constants/ce/_constants.ts
+++ Configuration/TypoScript/Base/Config/constants/ce/_constants.ts
@@ -1,2 +1,3 @@
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/Config/constants/ce/headlineimage.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/Config/constants/ce/jumboteaser.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/Config/constants/ce/teasercard.ts">
--- Configuration/TypoScript/Base/lib/contentElement/settings/start/ce.ts
+++ Configuration/TypoScript/Base/lib/contentElement/settings/start/ce.ts
@@ -15,9 +15,35 @@ lib {
 					// empty statement for proper comments only
 				ce {
 				}
-					// jumboteaser, teasercard
+					// headlineimage, jumboteaser, teasercard
 				ce =
 				ce {
+						// empty statement for proper comments only
+					headlineimage {
+					}
+						// css, html
+					headlineimage =
+					headlineimage {
+							// empty statement for proper comments only
+						css {
+						}
+							// plusouterwrap, subtitle, title
+						css =
+						css {
+							plusouterwrap	= {$start.ce.headlineimage.css.plusouterwrap}
+							subtitle			= {$start.ce.headlineimage.css.subtitle}
+							title					= {$start.ce.headlineimage.css.title}
+						}
+							// empty statement for proper comments only
+						html {
+						}
+							// tagsubtitle, tagtitle
+						html =
+						html {
+							tagsubtitle		= {$start.ce.headlineimage.html.tagsubtitle}
+							tagtitle			= {$start.ce.headlineimage.html.tagtitle}
+						}
+					}
 						// empty statement for proper comments only
 					jumboteaser {
 					}
@@ -27,11 +53,22 @@ lib {
 							// empty statement for proper comments only
 						css {
 						}
-							// button, plusouterwrap
+							// button, plusouterwrap, subtitle, title
 						css =
 						css {
 							button				= {$start.ce.jumboteaser.css.button}
 							plusouterwrap	= {$start.ce.jumboteaser.css.plusouterwrap}
+							subtitle			= {$start.ce.jumboteaser.css.subtitle}
+							title					= {$start.ce.jumboteaser.css.title}
+						}
+							// empty statement for proper comments only
+						html {
+						}
+							// tagsubtitle, tagtitle
+						html =
+						html {
+							tagsubtitle		= {$start.ce.jumboteaser.html.tagsubtitle}
+							tagtitle			= {$start.ce.jumboteaser.html.tagtitle}
 						}
 					}
 						// empty statement for proper comments only
@@ -43,13 +80,29 @@ lib {
 							// empty statement for proper comments only
 						css {
 						}
-							// button, panel, panelbutton, plusouterwrap
+							// button, panel, panelbutton, panelsubtitle, paneltitle, plusouterwrap, subtitle, title
 						css =
 						css {
 							button				= {$start.ce.teasercard.css.button}
+							divbutton			= {$start.ce.teasercard.css.divbutton}
 							panel					= {$start.ce.teasercard.css.panel}
 							panelbutton		= {$start.ce.teasercard.css.panelbutton}
+							panelsubtitle	= {$start.ce.teasercard.css.panelsubtitle}
+							paneltitle		= {$start.ce.teasercard.css.paneltitle}
 							plusouterwrap	= {$start.ce.teasercard.css.plusouterwrap}
+							subtitle			= {$start.ce.teasercard.css.subtitle}
+							title					= {$start.ce.teasercard.css.title}
+						}
+							// empty statement for proper comments only
+						html {
+						}
+							// paneltagsubtitle, paneltagtitle, tagsubtitle, tagtitle
+						html =
+						html {
+							paneltagsubtitle	= {$start.ce.teasercard.html.paneltagsubtitle}
+							paneltagtitle			= {$start.ce.teasercard.html.paneltagtitle}
+							tagsubtitle				= {$start.ce.teasercard.html.tagsubtitle}
+							tagtitle					= {$start.ce.teasercard.html.tagtitle}
 						}
 					}
 				}
