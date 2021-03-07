f3d177b88fa68508769b6425b9135f0b07a0f240
Dirk Wildt <wildt@die-netzmacher.de>
2021-03-07 22:43:33 +0100

    [TASK] ExtensionWrapper is extended for browser, caddy, org, quick_shop, slick, tt_news

--- Classes/Userfunc/ExtensionWrapper.php
+++ Classes/Userfunc/ExtensionWrapper.php
@@ -7,15 +7,17 @@ declare(strict_types = 1);
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
+ * 
+ * Class is used by EXT:startcustomer too
  */
 
 /**
- * Extension condition
+ * ExtensionWrapper
  * 
  * @author    Dirk Wildt <http://wildt.at.die-netzmacher.de>
  * @package    TYPO3
  * @subpackage    start
- * @version  8.2.8
+ * @version  8.2.21
  * @since    8.0.1
  */
 
@@ -45,12 +47,79 @@ class ExtensionWrapper
 	/**
 	 * @return bool
 	 * 
+	 * @version 8.2.21
+	 * @since 8.2.21
+	 */
+	public static function isLoadedBrowser( ): bool
+	{
+		return self::isLoaded('powermail');
+	}
+
+	/**
+	 * @return bool
+	 * 
+	 * @version 8.2.21
+	 * @since 8.2.21
+	 */
+	public static function isLoadedCaddy( ): bool
+	{
+		return self::isLoaded('powermail');
+	}
+
+	/**
+	 * @return bool
+	 * 
+	 * @version 8.2.21
+	 * @since 8.2.21
+	 */
+	public static function isLoadedOrg( ): bool
+	{
+		return self::isLoaded('powermail');
+	}
+
+	/**
+	 * @return bool
+	 * 
 	 * @version 8.2.8
 	 * @since 8.2.8
 	 */
 	public static function isLoadedPowermail( ): bool
 	{
 		return self::isLoaded('powermail');
+	}
+	
+	/**
+	 * @return bool
+	 * 
+	 * @version 8.2.21
+	 * @since 8.2.21
+	 */
+	public static function isLoadedQuickshop( ): bool
+	{
+		return self::isLoaded('powermail');
+	}
+	
+	/**
+	 * @return bool
+	 * 
+	 * @version 8.2.21
+	 * @since 8.2.21
+	 */
+	public static function isLoadedSlick( ): bool
+	{
+		return self::isLoaded('powermail');
+	}
+
+
+	/**
+	 * @return bool
+	 * 
+	 * @version 8.2.21
+	 * @since 8.2.21
+	 */
+	public static function isLoadedTtnews( ): bool
+	{
+		return self::isLoaded('tt_news');
 	}
 
 	/**
