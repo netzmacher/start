3c0441dc05bf926a52c978a0c46ffb8ff91a6a07
Dirk Wildt <wildt@die-netzmacher.de>
2021-05-23 20:16:26 +0200

    [TASK] Make TypoScript conditions compatible with TYPO3 8.7 and 10.4 #1182

---
+++ Configuration/TypoScript/Base/087/page/63146.ts
@@ -0,0 +1,12 @@
+page {
+		// Empty statement for propper comments only
+	63146 {
+	}
+	  // EXT:t3foundation: userFunc = Netzmacher\T3foundation\Userfunc\ContentUserfunc->WarnIfisGreaterThan095
+	63146 = USER
+	63146 {
+		userFunc = Netzmacher\T3foundation\Userfunc\ContentUserfunc->WarnIfisGreaterThan095
+		currentTS	= Start [01.1.1] (TYPO3 8.7) 
+		propperTS	= Start [01.1.1]
+	}
+}
