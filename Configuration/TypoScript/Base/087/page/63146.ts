e6d743ba70ef421004e83f0dd70d8e3c3972cec5
Dirk Wildt <wildt@die-netzmacher.de>
2021-05-29 18:33:27 +0200

    [BUGFIX] Netzmacher\T3foundation\Userfunc\ContentUserfunc -> Netzmacher\Start\Userfunc\ContentUserfunc

--- Configuration/TypoScript/Base/087/page/63146.ts
+++ Configuration/TypoScript/Base/087/page/63146.ts
@@ -2,10 +2,10 @@ page {
 		// Empty statement for propper comments only
 	63146 {
 	}
-	  // EXT:t3foundation: userFunc = Netzmacher\T3foundation\Userfunc\ContentUserfunc->WarnIfisGreaterThan095
+	  // EXT:start: userFunc = Netzmacher\Start\Userfunc\ContentUserfunc->WarnIfisGreaterThan095
 	63146 = USER
 	63146 {
-		userFunc = Netzmacher\T3foundation\Userfunc\ContentUserfunc->WarnIfisGreaterThan095
+		userFunc = Netzmacher\Start\Userfunc\ContentUserfunc->WarnIfisGreaterThan095
 		currentTS	= Start [01.1.1] (TYPO3 8.7) 
 		propperTS	= Start [01.1.1]
 	}
--- Configuration/TypoScript/Base/104/page/63146.ts
+++ Configuration/TypoScript/Base/104/page/63146.ts
@@ -2,10 +2,10 @@ page {
 		// Empty statement for propper comments only
 	63146 {
 	}
-	  // EXT:t3foundation: userFunc = Netzmacher\T3foundation\Userfunc\ContentUserfunc->WarnIfisSmallerThan100
+	  // EXT:start: userFunc = Netzmacher\Start\Userfunc\ContentUserfunc->WarnIfisSmallerThan100
 	63146 = USER
 	63146 {
-		userFunc = Netzmacher\T3foundation\Userfunc\ContentUserfunc->WarnIfisSmallerThan100
+		userFunc = Netzmacher\Start\Userfunc\ContentUserfunc->WarnIfisSmallerThan100
 		currentTS	= Start [01.1.1]
 		propperTS	= Start [01.1.1] (TYPO3 8.7)
 	}
