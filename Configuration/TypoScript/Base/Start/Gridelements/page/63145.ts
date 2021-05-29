a50da6ae02df70eda6e30d7bfb1588edb5c06498
Dirk Wildt <wildt@die-netzmacher.de>
2021-05-29 19:31:30 +0200

    [TASK] Prompt for deprectead template

---
+++ Configuration/TypoScript/Base/Start/Gridelements/page/63145.ts
@@ -0,0 +1,17 @@
+page {
+		// Empty statement for propper comments only
+	63145 {
+	}
+	63145 = TEXT
+	63145 {
+		value (
+			<div style="background:red;color:white;font-weight:bold;padding:1em;text-align:center;">
+				You are using a deprecated TypoScript Template<br />
+				Please move the template "Start [DEPRECATED!] +Gridelements"<br />
+				to "Start [20.4.1] Gridelements" depending on your responsive framework.<br />
+				<br />
+				This is a prompt by the EXT:start. Sorry for the trouble.
+			</div>
+)
+	}
+}
---
+++ Configuration/TypoScript/Base/Start/Gridelements/page/_setup.ts
@@ -0,0 +1,1 @@
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TypoScript/Base/Start/Gridelements/page/63145.ts">
