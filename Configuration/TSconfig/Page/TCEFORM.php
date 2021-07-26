1f3d392c7be2eee97d2f2124b8048d4c2c14eea2
Dirk Wildt <wildt@die-netzmacher.de>
2021-07-26 19:26:56 +0200

    [BUGFIX] TYPO3 8.7: Displays image manipulation for default, tablet and smartphone

--- Configuration/TSconfig/Page/TCEFORM.php
+++ Configuration/TSconfig/Page/TCEFORM.php
@@ -1,13 +1,9 @@
 <?php
 
 use Netzmacher\Start\Backend\Extensionmanager;
-use Netzmacher\Start\Userfunc\Typo3VersionUserfunc;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 if( !Extensionmanager::getProperty( 'responsiveimagesDisabled' ) )
 {
-	if( !Typo3VersionUserfunc::isSmallerThan104() )
-	{
-		ExtensionManagementUtility::addPageTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TSconfig/Page/TCEFORM/sys_file_reference/crop.ts">' );
-	}
+	ExtensionManagementUtility::addPageTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:start/Configuration/TSconfig/Page/TCEFORM/sys_file_reference/crop.ts">' );
 }
