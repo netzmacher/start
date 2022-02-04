8c02c623ce37e0a82b4b30be7be8cfbfef5d83d7
Dirk Wildt <dirk.wildt@die-netzmacher.de>
2022-02-04 14:21:41 +0100

    [TASK] request.getQueryParams() > traverse(request.getQueryParams()) #1927

--- Configuration/TypoScript/Base/104/param/language.ts
+++ Configuration/TypoScript/Base/104/param/language.ts
@@ -1,4 +1,4 @@
-[request.getQueryParams()['L'] == "{$start.config.language1.sys_language_uid}"]
+[traverse(request.getQueryParams())['L'] == "{$start.config.language1.sys_language_uid}"]
 	config {
 		htmlTag_langKey   = {$start.config.language1.htmlTag_langKey}
 		htmlTag_setParams	= {$start.config.language1.htmlTag_setParams}
@@ -8,7 +8,7 @@
 	}
 [global]
 
-[request.getQueryParams()['L'] == "{$start.config.language2.sys_language_uid}"]
+[traverse(request.getQueryParams())['L'] == "{$start.config.language2.sys_language_uid}"]
 	config {
 		htmlTag_langKey   = {$start.config.language2.htmlTag_langKey}
 		htmlTag_setParams	= {$start.config.language2.htmlTag_setParams}
